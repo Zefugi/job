e8bddd7850c2dc6b43ca32856ddcaad81a173d79
Zefugi <trowi@live.dk>
2020-10-18 11:23:53 +0200

    Created a JobSystemException class.

---
+++ Zefugi.JobSystem/Zefugi.JobSystem/JobSystemException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.JobSystem
+{
+    public class JobSystemException : Exception
+    {
+        public JobSystemException(string message) : base(message) { }
+    }
+}
