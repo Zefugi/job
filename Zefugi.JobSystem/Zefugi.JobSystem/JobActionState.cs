11fcce19fc96e4181c85cd7567c522460d762cbe
Zefugi <trowi@live.dk>
2020-10-18 11:52:32 +0200

    Created a JobActionState enum.

---
+++ Zefugi.JobSystem/Zefugi.JobSystem/JobActionState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.JobSystem
+{
+    public enum JobActionState
+    {
+        Unassigned,
+        Assigned,
+        Active,
+        Paused,
+        Completed,
+    }
+}
