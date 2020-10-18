925b774f005db12d7b158b6cf9a1ac87beea0f22
Zefugi <trowi@live.dk>
2020-10-18 10:47:07 +0200

    Created the class diagram.

---
+++ Zefugi.JobSystem/Zefugi.JobSystem/JobActionBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.JobSystem
+{
+    public class JobActionBase
+    {
+        protected virtual void OnAssigned() { }
+        protected virtual void OnCancel() { }
+        protected virtual void OnStart() { }
+        protected virtual void OnPause() { }
+        protected virtual void OnPanic() { }
+        protected virtual void OnResume() { }
+        protected virtual JobActionBase OnUpdate() { return null; }
+    }
+}
