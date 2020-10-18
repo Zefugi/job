9bcc71c7616a3d650fe38d23e2ec1acc9861283f
Zefugi <trowi@live.dk>
2020-10-18 10:51:06 +0200

    Listed the basic tests.

--- Zefugi.JobSystem/Zefugi.JobSystem.Tests/JobSystem_Tests.cs
+++ Zefugi.JobSystem/Zefugi.JobSystem.Tests/JobSystem_Tests.cs
@@ -19,5 +19,23 @@ namespace Zefugi.JobSystem.Tests
 
             Assert.IsTrue(jobs.Jobs.Contains(action));
         }
+        // TODO Assign also triggers OnAssigned
+
+        // TODO Cancel removes the action from Jobs and CurrentJob
+        // TODO Cancel also triggers OnCancel
+
+        // TODO Start pauses the current action and makes the new action current.
+        // TODO Start also triggers OnStart
+
+        // TODO Pause clears the current action.
+        // TODO Pause also triggers OnPause
+
+        // TODO Panic clears the current action.
+        // TODO Panic also triggers OnPanic.
+
+        // TODO Resume pauses the current action and sets a new current.
+        // TODO Resume also triggers OnResume.
+
+        // TODO Update triggers OnUpdate.
     }
 }
