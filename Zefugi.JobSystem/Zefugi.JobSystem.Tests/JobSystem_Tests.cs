f6071c048caedae04935601104e92d5fdb83860e
Zefugi <trowi@live.dk>
2020-10-18 10:46:57 +0200

    Wrote and passed Assign_MakesTheSpecifiedJobActionAvailableInJobs

---
+++ Zefugi.JobSystem/Zefugi.JobSystem.Tests/JobSystem_Tests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.JobSystem.Tests
+{
+    [TestFixture]
+    public class JobSystem_Tests
+    {
+        [Test]
+        public void Assign_MakesTheSpecifiedJobActionAvailableInJobs()
+        {
+            var jobs = new JobSystem();
+            var action = new JobActionBase();
+            jobs.Assign(action);
+
+            Assert.IsTrue(jobs.Jobs.Contains(action));
+        }
+    }
+}
---
+++ Zefugi.JobSystem/Zefugi.JobSystem/JobSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.JobSystem
+{
+    public class JobSystem
+    {
+        private List<JobActionBase> _jobs = new List<JobActionBase>();
+        private JobActionBase _currentJob;
+
+        public IReadOnlyList<JobActionBase> Jobs => _jobs.AsReadOnly();
+        public JobActionBase CurrentJob => _currentJob;
+
+        public void Assign(JobActionBase action)
+        {
+            _jobs.Add(action);
+        }
+    }
+}
